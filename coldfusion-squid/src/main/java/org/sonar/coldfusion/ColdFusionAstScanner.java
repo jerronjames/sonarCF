7ea636ce195502eebe3bf276630f4a2ca47a2c5b
jerronjames <jerronjames@users.noreply.github.com>
2014-07-21 14:26:51 -0600

    Moved CFCommentAnalyser from AstScanner to it's own file.

--- coldfusion-squid/src/main/java/org/sonar/coldfusion/ColdFusionAstScanner.java
+++ coldfusion-squid/src/main/java/org/sonar/coldfusion/ColdFusionAstScanner.java
@@ -46,35 +46,7 @@ import java.io.File;
 import java.util.Collection;
 
 public final class ColdFusionAstScanner {
-
-  private static class CFCommentAnalyser extends CommentAnalyser {
-     @Override
-    public boolean isBlank(String line) {
-      for (int i = 0; i < line.length(); i++) {
-        if (Character.isLetterOrDigit(line.charAt(i))) {
-          return false;
-        }
-      }
-      return true;
-    }
-
-    @Override
-    public String getContents(String comment) {
-      if (comment.startsWith("//")) {
-        return comment.substring(2);
-      } else if (comment.startsWith("/*")) {
-        return comment.substring(2, comment.length() - 2);
-      } else if (comment.startsWith("<!--")) {
-        if (comment.endsWith("-->")) {
-          return comment.substring(4, comment.length() - 3);
-        }
-        return comment.substring(4);
-      } else {
-        throw new IllegalArgumentException();
-      }
-    }
-  }
-
+  
   private ColdFusionAstScanner() {
   }
 
