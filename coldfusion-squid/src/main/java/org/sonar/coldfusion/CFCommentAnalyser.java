aacfc3dd3dee63398c70a254b5df796fe4d67af1
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2014-07-22 09:52:37 +0200

    Add the dropped code of CFCommentAnalyser back

--- coldfusion-squid/src/main/java/org/sonar/coldfusion/CFCommentAnalyser.java
+++ coldfusion-squid/src/main/java/org/sonar/coldfusion/CFCommentAnalyser.java
@@ -25,14 +25,28 @@ public class CFCommentAnalyser extends CommentAnalyser {
 
   @Override
   public boolean isBlank(String line) {
-    // FIXME
-    return false;
+    for (int i = 0; i < line.length(); i++) {
+      if (Character.isLetterOrDigit(line.charAt(i))) {
+        return false;
+      }
+    }
+    return true;
   }
 
   @Override
   public String getContents(String comment) {
-    // FIXME
-    return comment;
+    if (comment.startsWith("//")) {
+      return comment.substring(2);
+    } else if (comment.startsWith("/*")) {
+      return comment.substring(2, comment.length() - 2);
+    } else if (comment.startsWith("<!--")) {
+      if (comment.endsWith("-->")) {
+        return comment.substring(4, comment.length() - 3);
+      }
+      return comment.substring(4);
+    } else {
+      throw new IllegalArgumentException();
+    }
   }
 
 }
